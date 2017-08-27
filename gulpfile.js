a28ed46aaaeca9c1eab9b155b25d5fe2c9553b9e
Nathachai Thongniran <inid3a@gmail.com>
2017-08-27 21:12:33 +0700

    Remove unused

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,5 @@
 var gulp        = require('gulp');
-var notify      = require('gulp-notify'); // unused
 var browserSync = require('browser-sync').create();
-
-/*================================================================
- # HELPER
- ================================================================*/
-
-// unused
-function handleError(err) {
-  var msg = 'Error: ' + err.message;
-
-  console.error('Error', err.message);
-  browserSync.notify('Error: ' + err.message);
-
-  var args = Array.prototype.slice.call(arguments);
-  notify.onError({
-    title: 'Compile Error',
-    message: '<%= error.message %>'
-  }).apply(this, args);
-
-  if (typeof this.emit === 'function') this.emit('end')
-}
-
-/*================================================================
- # TASK
- ================================================================*/
 
 gulp.task('serve', function() {
   browserSync.init({
